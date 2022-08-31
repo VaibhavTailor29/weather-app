9d4f89d1d3c302b5afa303dfb6b897458c3433d1
Tailor Vaibhav <vaibhavtailor1256@gmail.com>
2022-08-31 20:00:54 +0530

    First Commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const cityApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'GEODB_CITY_API_KEY',
+		'X-RapidAPI-Key': 'GEODB_API_KEY', //get from Rapid API
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -10,6 +10,6 @@ export const CITY_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
 
-export const WEATHER_API_KEY = "WEATHER_API_KEY"
+export const WEATHER_API_KEY = "WEATHER_API_KEY" //get from  "https://openweathermap.org/api"
 
 
